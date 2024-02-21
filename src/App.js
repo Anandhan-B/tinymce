87e6f6380f55c2badd6b511342c4cf9b3822a95e
Anandhan-B <144510508+Anandhan-B@users.noreply.github.com>
2024-02-21 06:18:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
 import Dashboard from './Components/Dashboard/Dashboard';
 import AdminDashboard from './Components/AdminDashboard/ADashboard';
-import ForgetPassword from './Components/Forget-Password/ForgetPassword'
+import ForgetPassword from './Components/Forget-Password/ForgetPassword';
+import Otp from './Components/Forget-Password/Otp';
+import Reset from './Components/Forget-Password/Reset';
 
 const router = createBrowserRouter([
   {
@@ -42,6 +44,14 @@ const router = createBrowserRouter([
   {
     path:"/forgot-password",
     element:<ForgetPassword/>
+  },
+  {
+    path:"/otp",
+    element:<Otp/>
+  },
+  {
+    path:"/reset",
+    element:<Reset/>
   },
 
 ]);
